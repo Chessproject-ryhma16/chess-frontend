62c6559d3483e4d7b07e920df3e61b21ad7014f7
Aleksi2000 <Sorri.Aleksi@gmail.com>
2022-12-06 21:12:32 +0200

    Queen toimii

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -213,6 +213,46 @@ export default class Reference {
         return false
     }
 
+    queenMove(initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean {
+        for(let i = 1; i < 8; i++) {
+            //Diagonal
+                let multiplierX // = (desiredPosition.x < initialPosition.x) ? -1 : 1
+                let multiplierY // = (desiredPosition.y < initialPosition.y) ? -1 : 1
+                
+                if(desiredPosition.x < initialPosition.x) {
+                    multiplierX = -1
+                } else if (desiredPosition.x > initialPosition.x) {
+                    multiplierX = 1
+                } else {
+                    //X oiawhegpwbeqg
+                    multiplierX = 0
+                }
+
+                if(desiredPosition.y < initialPosition.y) {
+                    multiplierY = -1
+                } else if (desiredPosition.y > initialPosition.y) {
+                    multiplierY = 1
+                } else {
+                    //X oiawhegpwbeqg
+                    multiplierY = 0
+                }
+
+                let passedPosition: Position = {x: initialPosition.x + (i * multiplierX), y: initialPosition.y + (i * multiplierY)}
+
+                if(samePosition(passedPosition, desiredPosition)) {
+                    if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+                        return true
+                    }
+                } else {
+                    if(this.tileIsOccupied(passedPosition, boardState)) {
+                        break
+                    }
+                }
+            }
+
+        return false
+    }
+
     isValidMove(initialPosition: Position, desiredPosition: Position, type: PieceType, team: TeamType, boardState: Piece[]) {
         let validMove = false
         switch(type) {
@@ -227,6 +267,12 @@ export default class Reference {
                 break
             case PieceType.ROOK:
                 validMove = this.rookMove(initialPosition, desiredPosition, team, boardState)
+                break
+            case PieceType.QUEEN:
+                validMove = this.queenMove(initialPosition, desiredPosition, team, boardState)
+                break
+            case PieceType.KING:
+                
         } 
 
         return validMove  
